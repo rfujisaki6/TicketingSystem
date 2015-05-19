d3a9df2145d8397d234ae837df9c8438dffeec37
Ryohei Fujisaki <RyoheiFujisaki@Ryohei-no-MacBook-Air.local>
2015-05-19 14:58:36 -0700

    Initial Commit

---
+++ GroupProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GroupProject
+//
+//  Created by Ryohei Fujisaki on 2015/05/19.
+//  Copyright (c) 2015年 Ryohei Fujisaki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
